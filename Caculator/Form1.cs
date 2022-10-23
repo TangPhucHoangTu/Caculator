d982c4ba4280771c1ce6fefd9c711ef432338691
tangphuchoangtu <hoangtu230302@gmail.com>
2022-10-23 14:26:08 +0700

    Them su kien button Cong

--- Caculator/Form1.cs
+++ Caculator/Form1.cs
@@ -16,5 +16,29 @@ namespace Caculator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btKetQua_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text != String.Empty && txtB.Text != String.Empty)
+            {
+                double a = double.Parse(txtA.Text);
+                double b = double.Parse(txtB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }
+        }
+
+        private void btTru_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a - b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
